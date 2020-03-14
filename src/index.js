6ff164b025bb8f94aaa6bebf477cf561388bf1c0
Raman N <roma.nareiko@gmail.com>
2020-03-15 02:33:00 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openedBrackets = [];
+  const closedBrackets = [];
+  const brackets = [];
+
+  const bracketType = {
+    opened: 0,
+    closed: 1
+  }
+
+  bracketsConfig.forEach(element => {
+    openedBrackets.push(element[0]);
+    closedBrackets.push(element[1]);  
+  });
+
+  function getBracketType(bracket){
+    if(openedBrackets.includes(bracket) && closedBrackets.includes(bracket) && !brackets.includes(bracket))
+      return bracketType.opened;
+    else if(openedBrackets.includes(bracket) && closedBrackets.includes(bracket) && brackets.includes(bracket))
+      return bracketType.closed;
+    else if(openedBrackets.includes(bracket))
+      return bracketType.opened;
+    else
+      return bracketType.closed;
+  }
+
+  function checkString(str){
+    for(let i = 0; i < str.length; i++) {
+      if(getBracketType(str[i]) === bracketType.opened)
+        brackets.push(str[i]);
+      else {
+        let last = brackets.pop();
+
+        if(!last)
+          return false;
+        else{
+          let element = bracketsConfig.find(element => {
+            return element[0] === last;
+          });
+
+          if(!element || element[1] !== str[i])
+            return false;
+        }
+      }      
+    }
+
+    return brackets.length === 0;
+  }
+
+  return checkString(str);
+} 
